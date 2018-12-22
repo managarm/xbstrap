bf5a28ba989e8c61ea8bdd830989c91bc10239ad
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2018-12-22 18:26:37 +0100

    Update setup.py for upload to PyPI

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@
 from setuptools import setup
 
 setup(name='xbstrap',
+	version='0.1',
 	scripts=['scripts/xbstrap'],
-	install_requires='pyyaml')
+	install_requires='pyyaml',
+	author='Alexander van der Grinten',
+	author_email='alexander.vandergrinten@gmail.com',
+	license='MIT',
+	url='https://github.com/managarm/xbstrap')
 
