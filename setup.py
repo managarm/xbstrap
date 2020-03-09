82faec876ecf851138febbc7b5c54594e70c35d3
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2020-03-09 19:02:19 +0100

    Bump version to 0.11

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class CompletionInstall(install):
 		install.run(self)
 
 setup(name='xbstrap',
-	version='0.10',
+	version='0.11',
 	scripts=['scripts/xbstrap'],
 	install_requires=[
 		'colorama',
