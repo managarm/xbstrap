baea286cc83cd0b16dae8f84c86b3ed9fdb4d138
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2021-04-02 17:51:52 +0200

    Bump to version 0.22

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class CompletionInstall(install):
 		install.run(self)
 
 setup(name='xbstrap',
-	version='0.21',
+	version='0.22',
 	packages=['xbstrap'],
 	package_data={
 		'xbstrap': ['schema.yml']
