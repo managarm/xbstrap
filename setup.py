f72b19da125935dfbceaccf7ed1ff2a6773cdf86
Kacper Słomiński <kacper.slominski72@gmail.com>
2024-01-24 00:15:59 +0100

    setup: bump version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ class CompletionInstall(install):
 
 setup(
     name="xbstrap",
-    version="0.27.1",
+    version="0.28",
     packages=find_packages(),
     package_data={"xbstrap": ["schema.yml"]},
     install_requires=[
