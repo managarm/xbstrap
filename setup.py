4a8bd7952aac9ffeaa1d7ba8e8c7c5e1c7a8b854
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2025-09-28 11:44:54 +0200

    Bump version to 0.34.1

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ class CompletionInstall(install):
 
 setup(
     name="xbstrap",
-    version="0.34",
+    version="0.34.1",
     packages=find_packages(),
     package_data={"xbstrap": ["schema.yml"]},
     install_requires=[
